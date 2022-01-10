b98f8b8ec1541d80f292e4d835d07d5be0bb1d47
katjacodes <katjacodes@gmail.com>
2022-01-10 16:04:58 +0000

    wire up list of penpals

--- home/views.py
+++ home/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+
+
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def penpals(request):
+    profiles = Item.objects.all()
+    context = {
+        'profiles': profiles
+    }
+    return render(request, 'home/penpals.html', context)
