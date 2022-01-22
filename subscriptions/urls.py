19a1ca20fb25d922e765da5d42980dee1be140b7
katjacodes <katjacodes@gmail.com>
2022-01-22 16:02:30 +0000

    continue building payment functionality

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='subscriptions-home'),
+    path('config/', views.stripe_config),
+    path('create-checkout-session/', views.create_checkout_session),
 ]
