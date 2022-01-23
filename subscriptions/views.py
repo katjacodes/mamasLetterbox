93ae3490f93aa02dd23b6e1e789544ebe826462c
katjacodes <katjacodes@gmail.com>
2022-01-23 18:35:16 +0000

    continue setting up Stripe payment functionality

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,21 +1,27 @@
 import stripe
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.http.response import JsonResponse
+from django.contrib.auth.models import User
+from django.http.response import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+from subscriptions.models import StripeCustomer
+
 # Create your views here.
+
 
 @login_required
 def home(request):
     return render(request, 'subscriptions/home.html')
+
 
 @csrf_exempt
 def stripe_config(request):
     if request.method == 'GET':
         stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
         return JsonResponse(stripe_config, safe=False)
+
 
 @csrf_exempt
 def create_checkout_session(request):
@@ -39,3 +45,54 @@ def create_checkout_session(request):
             return JsonResponse({'sessionId': checkout_session['id']})
         except Exception as e:
             return JsonResponse({'error': str(e)})
+
+
+@login_required
+def success(request):
+    return render(request, 'success.html')
+
+
+@login_required
+def cancel(request):
+    return render(request, 'cancel.html')
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    endpoint_secret = settings.STRIPE_ENDPOINT_SECRET
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        # Fetch all the required data from session
+        client_reference_id = session.get('client_reference_id')
+        stripe_customer_id = session.get('customer')
+        stripe_subscription_id = session.get('subscription')
+
+        # Get the user and create a new StripeCustomer
+        user = User.objects.get(id=client_reference_id)
+        StripeCustomer.objects.create(
+            user=user,
+            stripeCustomerId=stripe_customer_id,
+            stripeSubscriptionId=stripe_subscription_id,
+        )
+        print(user.username + ' just subscribed.')
+
+    print('Success!')
+    return HttpResponse(status=200)
