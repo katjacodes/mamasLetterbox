d60a8f493637ea31121127c52c0ccfe13d200dd4
katjacodes <katjacodes@gmail.com>
2022-01-27 14:19:28 +0000

    continue deployment process

--- mamasLetterbox/settings.py
+++ mamasLetterbox/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tiph1-knz9m&1@s&y*&85dm34jdke^^x+uj!e78i5(*i(9orak
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mamas-letterbox.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -109,13 +109,17 @@ WSGI_APPLICATION = 'mamasLetterbox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASE = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
