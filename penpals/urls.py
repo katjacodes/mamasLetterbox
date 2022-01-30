6e44743b91d58f4b1dad8af8974f24561ef04916
katjacodes <katjacodes@gmail.com>
2022-01-30 18:32:21 +0000

    add url to dislpay basic subscription details to subscribed users

--- penpals/urls.py
+++ penpals/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.penpal_list, name='penpal_list'),
+    path('basicdetails/', views.basicdetails, name='basicdetails'),
     path('me/', views.my_penpal_profile, name='penpal_me'),
     path('me/edit/', views.my_penpal_profile_edit, name='penpal_me_edit'),
     path('create/', views.penpal_create, name='penpal_create'),
-    path('<int:penpal_id>/', views.penpal_detail, name='penpal_detail'),
+    path('<int:penpal_id>', views.penpal_detail, name='penpal_detail'),
+    path('<delete/', views.penpal_delete, name='penpal_delete'),
 ]
