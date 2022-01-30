0ff42a74df5dddd29985cdf649b93202d05cd812
katjacodes <katjacodes@gmail.com>
2022-01-30 18:29:12 +0000

    add subscription details view for subscribed users

--- penpals/views.py
+++ penpals/views.py
@@ -3,12 +3,20 @@ Code taken from Code Institute's Hello Django module and edited to fit project n
 """
 
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
 from .models import PenpalProfile
 from .forms import PenpalForm
 from subscriptions.decorators import subscription_required
+
+
+@login_required
+def basicdetails(request):
+    penpals = PenpalProfile.objects.all()
+    context = {'penpals': penpals}
+    return render(request, 'penpals/basic_details.html', context)
 
 
 @subscription_required
@@ -22,7 +30,7 @@ def penpal_list(request):
 def my_penpal_profile(request):
     if hasattr(request.user, 'penpal'):
         context = {'penpal': request.user.penpal}
-        return render(request, 'penpals/penpal_detail.html', context)
+        return render(request, 'penpals/my_details.html', context)
     else:
         return redirect(reverse('penpal_create'))
 
@@ -57,10 +65,19 @@ def penpal_create(request):
         form = PenpalForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('penpal_list')
+            return redirect('penpal_me')
     else:
         form = PenpalForm()
     context = {
         'form': form,
     }
     return render(request, 'penpals/penpal_create.html', {'form': form})
+
+@login_required
+def penpal_delete(request):
+    if hasattr(request.user, 'penpal'):
+        penpal = request.user.penpal
+        penpal.delete()
+        messages.success(request, f'Penpal profile successfully deleted!')
+    else:
+        return redirect(reverse('penpal_create'))
