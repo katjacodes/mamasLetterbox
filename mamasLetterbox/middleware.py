343d22e525c5c47d9e623571902e389858679da5
katjacodes <katjacodes@gmail.com>
2022-01-31 09:30:28 +0000

    add code attribution

--- mamasLetterbox/middleware.py
+++ mamasLetterbox/middleware.py
@@ -1,10 +1,14 @@
+"""
+Additional middleware
+"""
+
 from django.utils.deprecation import MiddlewareMixin
 
 
 class FixForwardedHostMiddleware(MiddlewareMixin):
     """
     Middleware to fix a CSRF error when running
-    the site locally. Code attribution: Benoit Blanchon
+    the site locally. Code attribution: Benoit Blanchon.
     """
 
     def process_request(self, request):
