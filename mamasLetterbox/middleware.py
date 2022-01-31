3284d5d591fabd6338efba8dad3407cef0df49b6
katjacodes <katjacodes@gmail.com>
2022-01-31 10:20:40 +0000

    update middleware

--- mamasLetterbox/middleware.py
+++ mamasLetterbox/middleware.py
@@ -17,7 +17,6 @@ class FixForwardedHostMiddleware(MiddlewareMixin):
         """
 
         forwarded_host = request.META.get('HTTP_X_FORWARDED_HOST')
-        print("forwarded host", forwarded_host)
         if forwarded_host:
             # remove port from host:port
             forwarded_host = forwarded_host.split(':')[0]
