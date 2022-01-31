eedeabc5220a788df19d8113a460255c576673bf
katjacodes <katjacodes@gmail.com>
2022-01-31 01:02:01 +0000

    code validation

--- penpals/apps.py
+++ penpals/apps.py
@@ -5,6 +5,6 @@ from django.apps import AppConfig
 
 class PenpalsConfig(AppConfig):
     """ 64-bit integer """
-    
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'penpals'
